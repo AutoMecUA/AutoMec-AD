64dcdf45468f10bf03c1b5dd73277b3ebd10b441
danc <danielsilveiracoelho@ua.pt>
2021-02-27 17:33:05 +0000

    new ML models and read video

--- robot_core/src/ML_approach/test.py
+++ robot_core/src/ML_approach/test.py
@@ -5,10 +5,14 @@ from datetime import datetime
 
 import csv
 
-with open('good_train_data/27_02_21__13_48_11.csv') as csv_file:
+with open('good_train_data/27_02_21__13_58_02.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    i=0
     for row in csv_reader:
         print(row[len(row)-1])
+        i+=1
+        if i==100:
+            break
 
     
