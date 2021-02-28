f5b2df066675336ab035e3b9d7302a58e28e4b5d
TiagoRSReis <reistiago14@ua.pt>
2021-02-28 15:00:23 +0000

    New csv for good data train

--- robot_core/src/ML_approach/ML_models/catboost_test.py
+++ robot_core/src/ML_approach/ML_models/catboost_test.py
@@ -8,21 +8,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #   Importing the dataset
-dataset1 = pd.read_csv('../good_train_data/27_02_21__13_48_11.csv')
-dataset2 = pd.read_csv('../good_train_data/27_02_21__13_58_02.csv')
+dataset1 = pd.read_csv('../good_train_data/27_02_21__13_48_11.csv') #200 linhas Driver: Daniel Coelho
+dataset2 = pd.read_csv('../good_train_data/27_02_21__13_58_02.csv') #600 linhas Driver: Daniel Coelho
+dataset3 = pd.read_csv('../good_train_data/28_02_21__12_18_50.csv') #600 linhas Driver: Tiago Reis
+dataset4 = pd.read_csv('../good_train_data/28_02_21__14_22_55.csv') #800 linhas Driver: Tiago Reis
+
 
 dataset1 = dataset1.drop(columns=['linear','angular','pixel.20400'])
 dataset2 = dataset2.drop(columns=['linear','angular','pixel.20400'])
+dataset3 = dataset3.drop(columns=['linear','angular','pixel.20400'])
+dataset4 = dataset4.drop(columns=['linear','angular','pixel.20400'])
 dataset = dataset1.append(dataset2)
+dataset_train = dataset2.append(dataset3)
+dataset_train = dataset_train.append(dataset4)
+
 print(dataset.shape)
 print(dataset1.shape)
 print(dataset2.shape)
+print(dataset_train.shape)
 
-X_train = dataset2.iloc[:,:-1]
-y_train = dataset2.iloc[:,-1]
+X_train = dataset_train.iloc[:,:-1]
+y_train = dataset_train.iloc[:,-1]
 
 X_test = dataset1.iloc[:,:-1]
 y_test = dataset1.iloc[:,-1]
+
 
 
 # Training the model
@@ -36,3 +46,4 @@ y_pred=regressor.predict(X_test)
 #   Evaluating the model performance
 from sklearn.metrics import r2_score
 print(r2_score(y_test,y_pred))
+print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
