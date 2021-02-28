162a7728265d8d48b1bd9d1bb0e0f731292c3cf3
TiagoRSReis <reistiago14@ua.pt>
2021-02-28 20:17:04 +0000

    New model

--- robot_core/src/ML_approach/ML_models/catboost_test.py
+++ robot_core/src/ML_approach/ML_models/catboost_test.py
@@ -12,15 +12,18 @@ dataset1 = pd.read_csv('../good_train_data/27_02_21__13_48_11.csv') #200 linhas 
 dataset2 = pd.read_csv('../good_train_data/27_02_21__13_58_02.csv') #600 linhas Driver: Daniel Coelho
 dataset3 = pd.read_csv('../good_train_data/28_02_21__12_18_50.csv') #600 linhas Driver: Tiago Reis
 dataset4 = pd.read_csv('../good_train_data/28_02_21__14_22_55.csv') #800 linhas Driver: Tiago Reis
+dataset5 = pd.read_csv('../good_train_data/28_02_21__19_42_15.csv') #6000 linhas Driver: Tiago Reis
 
 
 dataset1 = dataset1.drop(columns=['linear','angular','pixel.20400'])
 dataset2 = dataset2.drop(columns=['linear','angular','pixel.20400'])
 dataset3 = dataset3.drop(columns=['linear','angular','pixel.20400'])
 dataset4 = dataset4.drop(columns=['linear','angular','pixel.20400'])
+dataset5 = dataset5.drop(columns=['linear','angular','pixel.20400'])
 #dataset = dataset1.append(dataset2)
 #dataset_train = dataset2.append(dataset3)
-dataset_train = dataset3.append(dataset4)
+# dataset_train = dataset3.append(dataset4)
+dataset_train = dataset5
 
 #print(dataset.shape)
 print(dataset1.shape)
@@ -30,8 +33,10 @@ print(dataset_train.shape)
 X_train = dataset_train.iloc[:,:-1].values
 y_train = dataset_train.iloc[:,-1].values
 
-X_test = dataset1.iloc[:,:-1].values
-y_test = dataset1.iloc[:,-1].values
+# X_test = dataset1.iloc[:,:-1].values
+# y_test = dataset1.iloc[:,-1].values
+X_test = dataset4.iloc[:,:-1].values
+y_test = dataset4.iloc[:,-1].values
 
 
 
@@ -46,5 +51,5 @@ y_pred=regressor.predict(X_test)
 #   Evaluating the model performance
 from sklearn.metrics import r2_score
 print(r2_score(y_test,y_pred))
-regressor.save_model('catboost_file2')
+regressor.save_model('catboost_file3')
 print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
