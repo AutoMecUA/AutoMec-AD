2e3da5fc0f547f45c1f98e228ff835b5f1a93466
brunomonteiro98@ua.pt <brunomonteiro98@ua.pt>
2021-07-15 21:34:20 +0100

    joy2 test branch

---
+++ core/src/ManualDriving/ServoRos/ServoRos.ino
@@ -0,0 +1,67 @@
+/*
+ * rosserial Servo Control Example
+ *
+ * This sketch demonstrates the control of hobby R/C servos
+ * using ROS and the arduiono
+ * 
+ * For the full tutorial write up, visit
+ * www.ros.org/wiki/rosserial_arduino_demos
+ *
+ * For more information on the Arduino Servo Library
+ * Checkout :
+ * http://www.arduino.cc/en/Reference/Servo
+ */
+
+// !!!!!!!!!!!!!!!!!   IMPORTANTE   !!!!!!!!!!!
+// rosrun rosserial_python serial_node.py /dev/ttyACM1
+// rosrun rosserial_python serial_node.py /dev/ttyACM1
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include <WProgram.h>
+#endif
+
+#include <Servo.h> 
+#include <ros.h>
+#include <std_msgs/Int16.h>
+
+ros::NodeHandle  nh;
+
+Servo servo;
+Servo ESC;
+
+void servo_dir( const std_msgs::Int16 & cmd_msg){
+  servo.write(cmd_msg.data); //set servo angle, should be from 0-180  
+  //digitalWrite(LED_BUILTIN, HIGH-digitalRead(LED_BUILTIN));  //toggle led  
+}
+
+void servo_vel( const std_msgs::Int16 & cmd_msg){
+  ESC.write(cmd_msg.data); //set servo angle, should be from 0-180  
+  //digitalWrite(LED_BUILTIN, HIGH-digitalRead(LED_BUILTIN));  //toggle led  
+}
+
+
+ros::Subscriber<std_msgs::Int16> sub_dir("pub_dir", servo_dir);
+ros::Subscriber<std_msgs::Int16> sub_vel("pub_vel", servo_vel);
+
+void setup(){
+  //pinMode(LED_BUILTIN, OUTPUT);
+
+  nh.initNode();
+  nh.subscribe(sub_dir);
+  nh.subscribe(sub_vel);
+  
+  servo.attach(9); //attach it to pin 9
+  ESC.attach(6);
+  ESC.write(90);
+  delay(2000);
+}
+
+void loop(){
+  nh.spinOnce();
+  delay(1);
+} 
+
+
