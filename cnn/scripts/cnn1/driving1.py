bd823ac03e20c6dd356d29b831a9d55958f1ca94
Manuel <gomesnelito@gmail.com>
2021-08-04 18:24:54 +0100

    Resized image to follow standard

--- cnn/scripts/cnn1/driving1.py
+++ cnn/scripts/cnn1/driving1.py
@@ -27,7 +27,7 @@ def preProcess(img):
     #img = img[60:135, :, :]
     img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
     img = cv2.GaussianBlur(img,  (3, 3), 0)
-    img = cv2.resize(img, (200, 66))
+    img = cv2.resize(img, (320, 160))
     img = img/255
     return img
 
