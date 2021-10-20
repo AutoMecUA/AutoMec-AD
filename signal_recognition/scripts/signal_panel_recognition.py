771569d121b35d78281ef9e2d70ea58452a7e2f7
Manuel <gomesnelito@gmail.com>
2021-10-20 19:03:20 +0100

    Test creation of csv

--- signal_recognition/scripts/signal_panel_recognition.py
+++ signal_recognition/scripts/signal_panel_recognition.py
@@ -8,6 +8,10 @@ from std_msgs.msg import Bool
 from sensor_msgs.msg._Image import Image
 from cv_bridge.core import CvBridge
 import pathlib
+from datetime import datetime
+import pandas as pd
+import signal
+import sys
 
 global img_rbg
 global bridge
@@ -24,8 +28,17 @@ def message_RGB_ReceivedCallback(message):
     begin_img = True
 
 
-def main():
+def signal_handler(sig, frame):
+    global driving_log
+    global log_path
 
+    rospy.loginfo('You pressed Ctrl+C!')
+    driving_log.to_csv(log_path + '/driving_log.csv', mode='a', index=False, header=False)
+    sys.exit(0)
+
+def main():
+    global driving_log
+    global log_path
     # PARAMETERS__________________________________________________________________
 
     # Import Parameters
@@ -78,6 +91,17 @@ def main():
     
     # Create publishers
     pubbool = rospy.Publisher(signal_cmd_topic, Bool, queue_size=10)
+
+    # Define path for .csv
+    s = str(pathlib.Path(__file__).parent.absolute())
+    log_path = s + '/../log/'
+    rospy.loginfo(log_path)
+
+    # Create pandas dataframe
+    driving_log = pd.DataFrame(columns=['Time', 'Signal', 'Resolution'])
+
+    # set handler on termination
+    signal.signal(signal.SIGINT, signal_handler)
 
     # ______________________________________________________________________________
     
@@ -225,6 +249,15 @@ def main():
                     max_name = name
                     max_key = key
 
+
+        # Write log files
+        curr_time = datetime.datetime.now()
+        time_str = str(curr_time.year) + '_' + str(curr_time.month) + '_' + str(curr_time.day) + '__' + str(
+            curr_time.hour) + '_' + str(curr_time.minute) + '_' + str(curr_time.second) + '__' + str(
+            curr_time.microsecond)
+        # add image, angle and velocity to the driving_log pandas
+        row = pd.DataFrame([[time_str, max_name, max_res]], columns=['Time', 'Signal', 'Recognition'])
+        driving_log = driving_log.append(row, ignore_index=True)
 
         if max_res > detection_threshold:
 
