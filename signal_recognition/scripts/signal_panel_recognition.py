b4b5e5cc45a89905d32cb0222436a63ad6d30867
Manuel <gomesnelito@gmail.com>
2021-10-22 15:20:19 +0100

    Creates log file for signal recognition, with the signal detected and it's resolution

--- signal_recognition/scripts/signal_panel_recognition.py
+++ signal_recognition/scripts/signal_panel_recognition.py
@@ -30,15 +30,16 @@ def message_RGB_ReceivedCallback(message):
 
 
 def signal_handler(sig, frame):
-    global driving_log
+    global signal_log
     global log_path
 
     rospy.loginfo('You pressed Ctrl+C!')
-    driving_log.to_csv(log_path + '/driving_log.csv', mode='a', index=False, header=False)
+    signal_log.to_csv(log_path + '/signal_log.csv', mode='a', index=False, header=False)
+    print(signal_log)
     sys.exit(0)
 
 def main():
-    global driving_log
+    global signal_log
     global log_path
     # PARAMETERS__________________________________________________________________
 
@@ -99,11 +100,11 @@ def main():
 
     # Define path for .csv
     s = str(pathlib.Path(__file__).parent.absolute())
-    log_path = s + '/../log/'
+    log_path = s + '/log/'
     rospy.loginfo(log_path)
 
     # Create pandas dataframe
-    driving_log = pd.DataFrame(columns=['Time', 'Signal', 'Resolution'])
+    signal_log = pd.DataFrame(columns=['Time', 'Signal', 'Resolution'])
 
     # set handler on termination
     signal.signal(signal.SIGINT, signal_handler)
@@ -256,13 +257,13 @@ def main():
 
 
         # Write log files
-        curr_time = datetime.datetime.now()
+        curr_time = datetime.now()
         time_str = str(curr_time.year) + '_' + str(curr_time.month) + '_' + str(curr_time.day) + '__' + str(
             curr_time.hour) + '_' + str(curr_time.minute) + '_' + str(curr_time.second) + '__' + str(
             curr_time.microsecond)
-        # add image, angle and velocity to the driving_log pandas
-        row = pd.DataFrame([[time_str, max_name, max_res]], columns=['Time', 'Signal', 'Recognition'])
-        driving_log = driving_log.append(row, ignore_index=True)
+        # add image, angle and velocity to the signal_log pandas
+        row = pd.DataFrame([[time_str, max_name, max_res]], columns=['Time', 'Signal', 'Resolution'])
+        signal_log = signal_log.append(row, ignore_index=True)
         
         if max_res > detection_threshold:
 
