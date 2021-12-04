c7620507594e7106b96b0d91acb97dfbc1a56ae3
manuelgitgomes <gomesnelito@gmail.com>
2021-12-04 15:32:23 +0000

    #105 Faulty attempt to subscribe to camera info

--- ipm/scripts/ipm_optimized_ros.py
+++ ipm/scripts/ipm_optimized_ros.py
@@ -3,89 +3,89 @@
 # Imports
 import cv2
 import numpy as np
-import copy
-import numpy as np
-from numpy.lib.function_base import angle
 import rospy
-from geometry_msgs.msg._Twist import Twist
-from std_msgs.msg import Bool
-from sensor_msgs.msg._Image import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge.core import CvBridge
-from datetime import datetime
-import pandas as pd
-from ipm_optimized2 import IPM
-from sklearn import preprocessing
+from lib import ipm_class_ros
+
 global ipm
 global bridge
 global begin
-import cv2
+
 
 # Callback function to receive image
 def message_RGB_ReceivedCallback(message):
-    global img_rbg
-    global bridge
-    global begin_img
+    # Defining global variables
     global bridge
     global ipm
-    global begin    
+    global img_rgb
+    global output_image
+    global seeimage
 
+    # Transforming ROS image into CV2 image
+    img_rgb = bridge.imgmsg_to_cv2(message, "bgr8")
 
+    # Converting to grayscale
+    gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-    img_rbg = bridge.imgmsg_to_cv2(message, "bgr8")
-
-    print('in')
-    gray = cv2.cvtColor(img_rbg, cv2.COLOR_BGR2GRAY)
-
+    # Calculating output image
     output_image = ipm.calculate_output_image(gray)
 
-    cv2.imshow('initial_image', img_rbg)
-    cv2.imshow('final_image', output_image.astype(np.uint8))
-    cv2.waitKey(1)
+    seeimage = True
 
 
 
+def message_Info_ReceivedCallback(message):
+    # Defining global variables
+    global K
+    global R
+    global height, width
+    global seeinfo
+
+    K = message.K
+    R = message.R
+    height = message.height
+    width = message.width
+    seeinfo = True
 
 
 def main():
-    global ipm
     global bridge
-    global begin
-    begin = True
+    global K
+    global R
+    global height, width
+    global ipm
+    global seeimage, seeinfo
+
+    # Defining variables
+    bridge = CvBridge()
+    cam_height = 0.547
+    seeimage = False
+    seeinfo = False
 
     # Init Node
     rospy.init_node('ipm', anonymous=False)
 
+    # Defining parameters
+    image_info_topic = rospy.get_param('~image_info_topic', '/ackermann_vehicle/camera/rgb/camera_info')
     image_raw_topic = rospy.get_param('~image_raw_topic', '/ackermann_vehicle/camera/rgb/image_raw')
 
-    dim = (400,300)
+    rospy.Subscriber(image_info_topic, CameraInfo, message_Info_ReceivedCallback)
 
-    config_intrinsic = {'fov_x' : 1.09,
-                        'fov_y' : 1.09,
-                        'img_dim' : dim}
+    # print(seeinfo)
 
-    bridge = CvBridge()
-
-    config_extrinsic = {'camera_height': 0.547,
-                        'yaw': 0.6}
-
-    ipm = IPM(config_intrinsic, config_extrinsic)
-
-    bridge = CvBridge()
-
-    rate = rospy.Rate(1)
-
-    while not rospy.is_shutdown():
+    if seeinfo:
+        rospy.loginfo('Received camera info')
+        ipm = ipm_class_ros.IPM(height=height, width=width, K=K, R=R, cam_height=cam_height)
         rospy.Subscriber(image_raw_topic, Image, message_RGB_ReceivedCallback)
-        rate.sleep()
-   
-        
 
+        if seeimage:
+            rospy.loginfo('Received camera image')
+            cv2.imshow('initial_image', img_rgb)
+            cv2.imshow('final_image', output_image.astype(np.uint8))
+            cv2.waitKey(1)
 
-  
-
-
-    
-
+    rospy.spin()
 
 
 if __name__ == "__main__":
