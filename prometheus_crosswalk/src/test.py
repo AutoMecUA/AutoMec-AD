3f5006c6f934fa1dca4d04be596d6acf91795b95
Almeida-a <almeida.a@ua.pt>
2022-08-13 17:34:38 +0100

    feat(crosswalk lib): #154 Added fps to frame description stdout line
    Other minor tweak: waitkey delay was changed from infinite to 1ms

--- prometheus_crosswalk/src/test.py
+++ prometheus_crosswalk/src/test.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import time
 
 import cv2
 from PIL.ImageSequence import Iterator
@@ -10,7 +11,7 @@ except ImportError:
     import lib
 
 # Toggle debug mode - features random frame skip
-DEBUG_MODE: bool = True
+DEBUG_MODE: bool = False
 FRAME_SKIP_RATE = 0.99
 VIDEO_FILE_PATH = "../crosswalk_cut.mp4"
 
@@ -42,14 +43,19 @@ def read_video(video_file_path: str) -> Iterator:
 
 def stateless_test(func: callable):
     for frame in read_video(video_file_path=VIDEO_FILE_PATH):
+        start_time = time.time()
         sureness = func(frame)
+        frames_per_second = 1 / (time.time() - start_time)
+
+        print(f"Current frame - {int(frames_per_second)} fps: ", end="")
         if sureness > 0.5:
-            print(f"Current frame: IS crosswalk ({int(sureness*100)}% sure)")
+            print(f"IS crosswalk ({int(sureness*100)}% sure)")
         else:
-            print(f"Current frame: NOT crosswalk (only {int(sureness * 100)}% sure)")
+            print(f"NOT crosswalk (only {int(sureness * 100)}% sure)")
+
         cv2.namedWindow(winname="Frame", flags=cv2.WINDOW_KEEPRATIO)
         cv2.imshow("Frame", frame)
-        cv2.waitKey()
+        cv2.waitKey(1)
 
 
 if __name__ == '__main__':
