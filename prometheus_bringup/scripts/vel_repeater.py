e1438c8d650cd70405ed8875258501debccdc72b
tbsauce <telmobelasauce@ua.pt>
2022-11-26 11:59:18 +0000

    #156 added velociy Repeater

--- prometheus_bringup/scripts/vel_repeater.py
+++ prometheus_bringup/scripts/vel_repeater.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python3
 
+
+from functools import partial
 from typing import Any
 from geometry_msgs.msg._Twist import Twist
+import rospy
 
 
-def modelSteeringCallback(message, config):
-    config['steering'] = message.data
+def twistCallBack(message, config):
+    config['twist'] = message.data
+
 
 def main():
     config: dict[str, Any] = dict(cmd_vel=None)
 
     # Defining starting values
     config["twist"] = Twist()
+
+    # Init Node
+    rospy.init_node('vel_repeater', anonymous=False)
+    
+    # Getting parameters
+    twist_cmd_topic = rospy.get_param('~image_cmd_topic', '/cmd_vel')
+    twist_temp_topic = rospy.get_param('~twist_temp_topic', '/cmd_vel_temp')
+    rate_int = rospy.get_param('~rate_int', '30')
+
+
+    # Partials
+    twistCallBack_part = partial(twistCallBack, config=config)
+
+    # Subscribe and publish topics
+    rospy.Subscriber(twist_temp_topic, Twist, twistCallBack_part)
+    twist_pub = rospy.Publisher(twist_cmd_topic, Twist, queue_size=10)
+
+    # Frames per second
+    rate = rospy.Rate(rate_int)
+    
+    while not rospy.is_shutdown:
+
+        # To avoid any errors
+        twist_pub.publish(config["twist"])
+
+
+        rate.sleep()
 
 
 if __name__ == '__main__':
