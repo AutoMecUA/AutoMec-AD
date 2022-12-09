9051d6bce75b806b32c6b845eb9ec688b001131b
brunofavs <bruno.favs@ua.pt>
2022-12-09 00:07:56 +0000

    #172 Cleaned code to work with classes, as its more concise and easier to read

--- prometheus_signal_recognition/scripts/signal_foot_recongnition.py
+++ prometheus_signal_recognition/scripts/signal_foot_recongnition.py
@@ -9,11 +9,17 @@ from std_msgs.msg import String, Header
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
-def subscriberCallback(image_msg,rgb_args):
 
-    rgb_args["cv_image"] = rgb_args['bridge'].imgmsg_to_cv2(image_msg,"bgr8") # Passthrough means wtv the encoding was before, it will be retained
+class image:
 
-    rospy.loginfo("Received image message, image shape is " + str(rgb_args["cv_image"].shape))
+    def __init__(self,image_args):
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/bottom_front_camera/rgb/image_color",Image,partial(self.subscriberCallback,image_args = image_args))
+
+    def subscriberCallback(self,image_msg,image_args = {}):
+
+        image_args["cv_image"] = self.bridge.imgmsg_to_cv2(image_msg,"bgr8") # Passthrough means wtv the encoding was before, it will be retained
+        rospy.loginfo("Received image message, image shape is " + str(image_args["cv_image"].shape))
 
     
 
@@ -22,19 +28,20 @@ def main():
     # Initialization
     # -----------------------------
     rospy.init_node("foot_recognition", anonymous=False)
-
-    rgb_args = dict(bridge = CvBridge(),cv_image = None)
-
-    rospy.Subscriber("/bottom_front_camera/rgb/image_color",Image,partial(subscriberCallback,rgb_args = rgb_args))
-
     cv2.namedWindow("CV_image")
+
+    image_args = dict(cv_image = 0) # Can't be none or imshow will yield error
+    bottom_front_camera = image(image_args)
+
+
     # -----------------------------
     # Processing
     # -----------------------------
     while(1):
-        cv2.imshow("CV_image",rgb_args["cv_image"])
+        # print(image_args["cv_image"])
+        cv2.imshow("CV_image",image_args["cv_image"])
         cv2.waitKey(1)    
-
+        pass
     # -----------------------------
     # Termination
     # -----------------------------
