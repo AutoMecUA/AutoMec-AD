66cba0df629f37865134b838ba7ab7c733c4ef53
brunofavs <bruno.favs@ua.pt>
2022-12-12 22:07:10 +0000

    #172 Coisas

--- prometheus_signal_recognition/scripts/signal_foot_recongnition.py
+++ prometheus_signal_recognition/scripts/signal_foot_recongnition.py
@@ -12,36 +12,64 @@ from cv_bridge import CvBridge
 
 class image:
 
-    def __init__(self,image_args):
+    #* Class variables-----
+
+    #* ---------------------
+    def __init__(self):
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/bottom_front_camera/rgb/image_color",Image,partial(self.subscriberCallback,image_args = image_args))
+        self.image_sub = rospy.Subscriber("/bottom_front_camera/rgb/image_color",Image,self.subscriberCallback)
+        self.image_args = {} # Contains cv_image
+        self.begin_image = False
 
-    def subscriberCallback(self,image_msg,image_args = {}):
+    def subscriberCallback(self,image_msg):
 
-        image_args["cv_image"] = self.bridge.imgmsg_to_cv2(image_msg,"bgr8") # Passthrough means wtv the encoding was before, it will be retained
-        rospy.loginfo("Received image message, image shape is " + str(image_args["cv_image"].shape))
+        self.begin_image = True # After receiving first image will be forever true
 
+        #! If I assign directly to cv_image key, it will show flickers of the non flipped image 
+        self.image_args["processing_image"] = self.bridge.imgmsg_to_cv2(image_msg,"bgr8") # Passthrough means wtv the encoding was before, it will be retained
+        self.flipImage('x','y')
+        rospy.loginfo("Received image message, image shape is " + str(self.image_args["cv_image"].shape))
+
+    def showImage(self):
+
+        if self.begin_image == False: # Only shows after first image is processed
+            return
+
+        cv2.namedWindow("CV_image")
+        cv2.imshow("CV_image",self.image_args["cv_image"])
+        pressed_key = cv2.waitKey(1) & 0xFF # To prevent NumLock issue
+
+        if pressed_key == ord('q'):
+            print("Quitting program")
+            cv2.destroyAllWindows
+            rospy.signal_shutdown("Order to quit") # Stops ros
+            exit()                                 # Exits python script
     
+    def flipImage(self,*args):
+        
+        for arg in args:
+            try:
+                if arg == 'x':
+                    self.image_args["processing_image"] = cv2.flip(self.image_args["processing_image"],1)
+                elif arg == 'y':
+                    self.image_args["processing_image"] = cv2.flip(self.image_args["processing_image"],0)
+            except :
+                print("Unallowed arguments, should be 'x' or 'y' ")
+
+        self.image_args["cv_image"] = self.image_args["processing_image"] # Should find better way to do this, if I change cv image in the loop it will flicker 
 
 def main():
     # -----------------------------
     # Initialization
     # -----------------------------
     rospy.init_node("foot_recognition", anonymous=False)
-    cv2.namedWindow("CV_image")
-
-    image_args = dict(cv_image = 0) # Can't be none or imshow will yield error
-    bottom_front_camera = image(image_args)
-
+    bottom_front_camera = image()
 
     # -----------------------------
     # Processing
     # -----------------------------
     while(1):
-        # print(image_args["cv_image"])
-        cv2.imshow("CV_image",image_args["cv_image"])
-        cv2.waitKey(1)    
-        pass
+        bottom_front_camera.showImage()
     # -----------------------------
     # Termination
     # -----------------------------
