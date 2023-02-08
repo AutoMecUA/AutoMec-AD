651a1bdd1724023cb6e563fc7eb16bffb80c0e1f
Tatiana <tatianaresende@ua.pt>
2023-02-08 18:48:30 +0000

    try points 2D to 3D

--- prometheus_signal_recognition/scripts/dataset_label.py
+++ prometheus_signal_recognition/scripts/dataset_label.py
@@ -6,11 +6,14 @@ from colorama import Fore, Style
 from typing import Any
 from functools import partial
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 from gazebo_msgs.msg._LinkStates import LinkStates
 from sensor_msgs.msg._CameraInfo import CameraInfo
 import tf
 from math import cos, sin, pi
-
+import cv2
+from cv_bridge.core import CvBridge
+from sensor_msgs.msg._Image import Image
 
 # Callback function to receive link states
 def poses_callback(message, config: dict):
@@ -22,7 +25,16 @@ def poses_callback(message, config: dict):
 # Callback function to receive camera info
 def parameter_camera_callback(message, config: dict):
     
-    config['parameter_camera'] = message.K
+    config['camera_matrix'] = message.K
+    config['camera_matrix'] = np.reshape(config['camera_matrix'], (3, 3))
+    config['camera_matrix'][0][2] = 320
+    config['camera_matrix'][1][2] = 240
+  
+
+# Callback function to receive image
+def imgRgbCallback(message, config: dict):
+    
+    config['img_rgb'] = config['bridge'].imgmsg_to_cv2(message, "rgb8")
 
 
 # Convert quaternion to roll, pitch, yaw
@@ -35,43 +47,90 @@ def quaternion2rpy(point_array):
     return roll, pitch, yaw
 
 
-# Calculate transformation matrix between two points
-def transformation_matrix(point_array_1,point_array_2):
-    """calculate transformation matrix between two points
-    Args:
-    point_array_1: [x,y,z,qx,qy,qz,qw]
-    point_array_2: [x,y,z,qx,qy,qz,qw]
-    """
-    point_array =  [point_array_1,point_array_2]
+# # Calculate transformation matrix between two points
+# def transformation_matrix(point_array_1,point_array_2):
+#     """calculate transformation matrix between two points
+#     Args:
+#     point_array_1: [x,y,z,qx,qy,qz,qw]
+#     point_array_2: [x,y,z,qx,qy,qz,qw]
+#     """
+#     point_array =  [point_array_1,point_array_2]
     
-    for number_point, point in enumerate(point_array,start=1):
+#     for number_point, point in enumerate(point_array,start=1):
         
-        roll, pitch, yaw = quaternion2rpy(point)
+#         roll, pitch, yaw = quaternion2rpy(point)
 
-        C00=cos(yaw)*cos(pitch)
-        C01=cos(yaw)*sin(pitch)*sin(roll) - sin(yaw)*sin(roll)
-        C02=cos(yaw)*sin(pitch)*cos(roll) + sin(yaw)*sin(roll)
-        C03=point[0]
-        C10=sin(yaw)*cos(pitch)
-        C11=sin(yaw)*sin(pitch)*sin(roll) + cos(yaw)*cos(roll)
-        C12=sin(yaw)*sin(pitch)*cos(roll) -cos(yaw)*sin(roll)
-        C13=point[1]
-        C20=-sin(pitch)
-        C21=cos(pitch)*sin(roll)
-        C22=cos(pitch)*cos(roll)
-        C23=point[2]
-        C30=0
-        C31=0
-        C32=0
-        C33=1
+#         C00=cos(yaw)*cos(pitch)
+#         C01=cos(yaw)*sin(pitch)*sin(roll) - sin(yaw)*sin(roll)
+#         C02=cos(yaw)*sin(pitch)*cos(roll) + sin(yaw)*sin(roll)
+#         C03=point[0]
+#         C10=sin(yaw)*cos(pitch)
+#         C11=sin(yaw)*sin(pitch)*sin(roll) + cos(yaw)*cos(roll)
+#         C12=sin(yaw)*sin(pitch)*cos(roll) -cos(yaw)*sin(roll)
+#         C13=point[1]
+#         C20=-sin(pitch)
+#         C21=cos(pitch)*sin(roll)
+#         C22=cos(pitch)*cos(roll)
+#         C23=point[2]
+#         C30=0
+#         C31=0
+#         C32=0
+#         C33=1
 
-        if number_point == 1:
-            obj1_mat=np.array([[C00, C01, C02, C03],[C10, C11, C12, C13],[C20, C21, C22, C23],[C30, C31, C32, C33]])
-        else:
-            obj2_mat=np.array([[C00, C01, C02, C03],[C10, C11, C12, C13],[C20, C21, C22, C23],[C30, C31, C32, C33]])
+#         if number_point == 1:
+#             obj1_mat=np.array([[C00, C01, C02, C03],[C10, C11, C12, C13],[C20, C21, C22, C23],[C30, C31, C32, C33]])
+#         else:
+#             obj2_mat=np.array([[C00, C01, C02, C03],[C10, C11, C12, C13],[C20, C21, C22, C23],[C30, C31, C32, C33]])
 
-    transformation_mat=np.dot(np.linalg.inv(obj2_mat), obj1_mat) #generating the transformation
-    return transformation_mat
+#     transformation_mat=np.dot(np.linalg.inv(obj2_mat), obj1_mat) #generating the transformation
+#     return transformation_mat
+
+
+def get_matrix(point_array,trans,rot):
+
+    r = R.from_quat(rot)
+    rot = r.as_matrix()
+
+    # homogeneous transformation matrix (4,4) 
+    l = 4
+    matrix_footprint2cam = np.zeros((l,l))
+    matrix_footprint2cam[0:3,0:3] = rot
+    matrix_footprint2cam[0:3,3] = trans
+    matrix_footprint2cam[3,3] = 1
+
+    # rotation matrix
+    r = R.from_quat([point_array[3], point_array[4], point_array[5], point_array[6]])
+    rot_matrix = r.as_matrix()
+    
+    # translation matrix
+    trans_matrix = np.array([point_array[0], point_array[1], point_array[2]], dtype=np.float32)
+
+    # homogeneous transformation matrix (4,4) 
+    matrix_world2footprint = np.zeros((l,l))
+    matrix_world2footprint[0:3,0:3] = rot_matrix
+    matrix_world2footprint[0:3,3] = trans_matrix
+    matrix_world2footprint[3,3] = 1
+
+    # print('matrix_world2footprint: \n', matrix_world2footprint)
+    # print('matrix_footprint2cam: \n', matrix_footprint2cam)
+    # r = R.from_matrix(matrix_footprint2cam[0:3,0:3])
+    # print(r.as_euler('zyx', degrees=True))
+    
+    matrix = np.dot(matrix_world2footprint,matrix_footprint2cam)
+    #print(matrix)
+    
+    # inverse matrices
+    matrix_inv = np.linalg.inv(matrix)
+
+
+    # r = R.from_matrix(matrix_inv[0:3,0:3])
+    # print(r.as_euler('zyx', degrees=True))
+    rot_matrix = matrix[0:3,0:3]
+    trans_matrix = matrix[0:3,3]
+    
+    # print(rot_matrix)
+    #print(trans_matrix)
+    return rot_matrix, trans_matrix
 
 
 # Calculate bounding box for each signal
@@ -105,7 +164,7 @@ def get_bounding_box(point):
     for T_center in vector_translation_center:
         point_T_center = [point[0]+T_center[0],point[1]+T_center[1],point[2]+T_center[2],point[3],point[4],point[5],point[6]]
         for T_corner in vector_translation_corner:
-            point_T_corner = [round(point_T_center[0]+T_corner[0],2),round(point_T_center[1]+T_corner[1],2),round(point_T_center[2]+T_corner[2],2),round(point_T_center[3],2),round(point_T_center[4],2),round(point_T_center[5],2),round(point_T_center[6],2)]
+            point_T_corner = [point_T_center[0]+T_corner[0],point_T_center[1]+T_corner[1],point_T_center[2]+T_corner[2],point_T_center[3],point_T_center[4],point_T_center[5],point_T_center[6]]
             bounding_box.append(point_T_corner)
     
     return bounding_box
@@ -117,7 +176,9 @@ def main():
     config: dict[str, Any] = dict(
         link_name=None,
         link_pose=None,
-        parameter_camera=None
+        camera_matrix=None,
+        img_rgb=None,
+        bridge=None
     )
 
     # print(f'{Fore.RED}Hello world! {Style.RESET_ALL}')
@@ -126,6 +187,7 @@ def main():
     rospy.init_node('label_data', anonymous=False)
 
     # Retrieving parameters
+    image_raw_topic = rospy.get_param('~image_raw_topic', '/top_right_camera/image_raw')
     rate_hz = rospy.get_param('~rate', 30)
   
     # Subscribers
@@ -133,9 +195,16 @@ def main():
     rospy.Subscriber('gazebo/link_states', LinkStates, PosesCallback_part)
     ParameterCameraCallback_part = partial(parameter_camera_callback, config=config)
     rospy.Subscriber('/top_right_camera/camera_info', CameraInfo, ParameterCameraCallback_part)
+    imgRgbCallback_part = partial(imgRgbCallback, config=config)
+    rospy.Subscriber(image_raw_topic, Image, imgRgbCallback_part)
     
+
+    # Create an object of the CvBridge class
+    config['bridge'] = CvBridge()
+
     # set loop rate
     rate = rospy.Rate(rate_hz)
+    listener = tf.TransformListener()
 
     while not rospy.is_shutdown():
         signal_poses = []
@@ -151,7 +220,7 @@ def main():
                     signal_pose['pose'] = config['link_pose'][i]
                     signal_poses.append(signal_pose)
 
-                elif name[1] == 'front_right_steer_link':
+                elif name[1] == 'base_footprint':
                     camera_pose = {}
                     camera_pose['signal'] = name[0]
                     camera_pose['pose'] = config['link_pose'][i]
@@ -159,21 +228,49 @@ def main():
             
             # Array of signals
             signal_pose_array = np.array([[pose['pose'].position.x,pose['pose'].position.y,pose['pose'].position.z,pose['pose'].orientation.x,pose['pose'].orientation.y,pose['pose'].orientation.z,pose['pose'].orientation.w] for pose in signal_poses],dtype = np.float64)
-
+            signal_pose_xyz = np.array([[pose['pose'].position.x,pose['pose'].position.y,pose['pose'].position.z] for pose in signal_poses],dtype = np.float64)
+            signal_name = np.array([pose['signal'] for pose in signal_poses])
+           
             # Array of cameras
-            camera_pose_array = np.array([[pose['pose'].position.x,pose['pose'].position.y,pose['pose'].position.z,pose['pose'].orientation.x,pose['pose'].orientation.y,pose['pose'].orientation.z,pose['pose'].orientation.w] for pose in camera_poses],dtype = np.float64)
-
-            # bbox = get_bounding_box(signal_pose_array[12])
-            # print(signal_pose_array[12])
-            # print(bbox)
-
-            #print(signal_pose_array)
-            #print(camera_pose_array)
-
-            transformation_mat=transformation_matrix(signal_pose_array[0],camera_pose_array[0])
+            base_footprint_pose_array = np.array([[pose['pose'].position.x,pose['pose'].position.y,pose['pose'].position.z,pose['pose'].orientation.x,pose['pose'].orientation.y,pose['pose'].orientation.z,pose['pose'].orientation.w] for pose in camera_poses],dtype = np.float64)
             
-            #print(transformation_mat)
-                
+        
+            bboxs = []
+            for i in range(len(signal_pose_array)):
+                bbox = get_bounding_box(signal_pose_array[i])
+                bboxs.append(bbox)
+
+            
+            try:
+                (trans,rot) = listener.lookupTransform('base_footprint', 'top_right_camera_rgb_optical_frame',  rospy.Time(0))
+            except (tf.LookupException, tf.ConnectivityException):
+                continue
+            
+            rot_matrix, trans_matrix = get_matrix(base_footprint_pose_array[0],trans,rot)
+
+            points_2d = cv2.projectPoints(signal_pose_xyz, rot_matrix, trans_matrix, config['camera_matrix'], None,)[0]
+            # homgeneous coordinates0
+      
+            # X = np.append(signal_pose_xyz[12],1)
+            # RT = np.hstack((rot_matrix,np.array([np.array(x, ndmin=1) for x in trans_matrix])))
+            # P = np.dot(config['camera_matrix'],RT)
+            # x_2d = np.dot(P,X)
+            # x_2d = x_2d/x_2d[-1]
+
+            
+            
+            print('signal: ' + signal_name[12] + ' point center:' + str(points_2d[12]))
+
+
+            # read opencv key
+            win_name = 'Robot View'
+            cv2.namedWindow(winname=win_name,flags=cv2.WINDOW_NORMAL)
+            image = cv2.cvtColor(config['img_rgb'], cv2.COLOR_BGR2RGB)
+
+            # print(image.shape)
+            # 480,640,3
+            cv2.imshow(win_name, image)
+            key = cv2.waitKey(1)
 
             
 
