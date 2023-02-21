be281b1fc25accac7e97f17249c7532a328ba54c
Andre <andref@ua.pt>
2023-02-21 20:02:54 +0000

    changed visualization to degrees

--- prometheus_driving/scripts/src/visualization.py
+++ prometheus_driving/scripts/src/visualization.py
@@ -53,7 +53,7 @@ class ClassificationVisualizer():
             ax.yaxis.set_ticklabels([])
             ax.xaxis.set_ticks([])
             ax.yaxis.set_ticks([])
-            ax.set_xlabel(f'{round(outputs[image_idx].data.item(),5)*180/pi} , {round(labels[image_idx].data.item(),5)*180/pi}', color=color)
+            ax.set_xlabel(f'{round(outputs[image_idx].data.item(),5)*180/pi}º , {round(labels[image_idx].data.item(),5)*180/pi}º', color=color)
 
         plt.draw()
         key = plt.waitforbuttonpress(0.05)
