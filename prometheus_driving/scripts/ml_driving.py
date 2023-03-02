4be172d39dd9989b6b89e83247a86ae0d5c34d39
Andre <andref@ua.pt>
2023-03-02 15:39:21 +0000

    added subscriber to change model used #165

--- prometheus_driving/scripts/ml_driving.py
+++ prometheus_driving/scripts/ml_driving.py
@@ -9,7 +9,7 @@ import numpy as np
 import rospy
 import yaml
 from sensor_msgs.msg._Image import Image
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32 , String
 from cv_bridge.core import CvBridge
 import torch
 from torchvision import transforms
@@ -47,6 +47,21 @@ def imgRgbCallback(message, config):
 
     config["begin_img"] = True
 
+def modelSteeringCallback(message, config):
+    model_name = message.data
+    automec_path = os.environ.get('AUTOMEC_DATASETS')
+    path = f'{automec_path}/models/{model_name}/{model_name}.pkl'
+    # Retrieving info from yaml
+    with open(f'{automec_path}/models/{model_name}/{model_name}.yaml') as file:
+        info_loaded = yaml.load(file, Loader=yaml.FullLoader)
+
+    rospy.loginfo('Using model: %s', path)
+    device = f'cuda:0' if torch.cuda.is_available() else 'cpu' # cuda: 0 index of gpu
+    model = eval(info_loaded['model']['ml_arch']['name'])
+    model = LoadModel(path,model,device)
+    model.eval()
+    config['model'] = model
+
 
 def main():
     config: dict[str, Any] = dict(vel=None, img_rgb=None,
@@ -75,9 +90,9 @@ def main():
 
     rospy.loginfo('Using model: %s', path)
     device = f'cuda:0' if torch.cuda.is_available() else 'cpu' # cuda: 0 index of gpu
-    model = eval(info_loaded['model']['ml_arch']['name'])
-    model= LoadModel(path,model,device)
-    model.eval()
+    config['model'] = eval(info_loaded['model']['ml_arch']['name'])
+    config['model'] = LoadModel(path,config['model'],device)
+    config['model'].eval()
 
     PIL_to_Tensor = transforms.Compose([
                     transforms.ToTensor()
@@ -86,8 +101,11 @@ def main():
     # Partials
     imgRgbCallback_part = partial(imgRgbCallback, config=config)
 
+    changeModelCallback = partial(modelSteeringCallback, config=config)
+
     # Subscribe and publish topics
     rospy.Subscriber(image_raw_topic, Image, imgRgbCallback_part)
+    rospy.Subscriber('/set_model', String, changeModelCallback)
     model_steering_pub = rospy.Publisher(model_steering_topic, Float32, queue_size=10)
 
     # Frames per second
@@ -106,7 +124,7 @@ def main():
         image = PIL_to_Tensor(image)
         image = image.unsqueeze(0)
         image = image.to(device, dtype=torch.float)
-        label_t_predicted = model.forward(image)
+        label_t_predicted = config['model'].forward(image)
         steering = float(label_t_predicted)
         # Publish angle
         model_steering_pub.publish(steering)
